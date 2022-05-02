224b6eedcb190bbb2a08957b36f30940163349a3
Nick Pizzigati <npizzigati@gmail.com>
2022-05-02 18:34:15 -0500

    Comment out database code to test heroku

--- app.rb
+++ app.rb
@@ -4,12 +4,7 @@ require 'sinatra'
 require 'pg'
 require 'erb'
 
-conn = PG.connect(:dbname => 'travelapp')
-conn.exec('SELECT * from recommendations') do |result|
-  result.each do |row|
-    puts row
-  end
-end
+# conn = PG.connect(:dbname => 'travelapp')
 
 get '/' do
   send_file 'index.html'
@@ -17,19 +12,20 @@ end
 
 get '/search' do
   place = params['place']
-  query = <<~QUERY
-    SELECT recommendation FROM recommendations
-     WHERE city = $1;
-  QUERY
-  template = <<~TEMPLATE
-    % results.each do |row|
-      <%= row['recommendation']%>
-    % end
-  TEMPLATE
-  results = conn.exec_params(query, [place])
-  results.each do |row|
-    puts row
-  end
-  output = ERB.new(template, trim_mode: "%<>")
-  output.result(binding)
+  # query = <<~QUERY
+  #   SELECT recommendation FROM recommendations
+  #    WHERE city = $1;
+  # QUERY
+  # template = <<~TEMPLATE
+  #   % results.each do |row|
+  #     <%= row['recommendation']%>
+  #   % end
+  # TEMPLATE
+  # results = conn.exec_params(query, [place])
+  # results.each do |row|
+  #   puts row
+  # end
+  # output = ERB.new(template, trim_mode: "%<>")
+  # output.result(binding)
+  place
 end
