b990be325e854dd1499d8dbf79f88b47cbedf6a3
Nick Pizzigati <npizzigati@gmail.com>
2022-05-09 18:06:40 -0500

    Add user name to query and display

--- app.rb
+++ app.rb
@@ -14,8 +14,10 @@ end
 get '/search' do
   place = params['place']
   query = <<~QUERY
-    SELECT body FROM reviews
-     WHERE city = $1 OR country = $1;
+    SELECT body, first_name, last_name
+    FROM   reviews
+    INNER JOIN users ON users.id = reviews.id
+    WHERE city = $1 OR country = $1;
   QUERY
   @results = conn.exec_params(query, [place])
   erb :place_search_results
