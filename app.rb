2ba12776c186976a851f12aa6dbce0dd03f8a854
Nick Pizzigati <npizzigati@gmail.com>
2022-05-30 19:47:20 -0500

    Change reviews table name to recommendation

--- app.rb
+++ app.rb
@@ -22,8 +22,8 @@ get '/search' do
   place = params['place']
   query = <<~QUERY
     SELECT body, title, first_name, last_name
-    FROM   reviews
-    INNER JOIN users ON users.id = reviews.user_id
+    FROM   recommendations
+    INNER JOIN users ON users.id = recommendations.user_id
     WHERE LOWER(city) = $1 OR LOWER(country) = $1;
   QUERY
   @results = conn.exec_params(query, [place.downcase])
