2f96d0f70a282d9671be10f6c0506cc909634c3d
Nick Pizzigati <npizzigati@gmail.com>
2022-06-12 18:35:52 -0500

    Minor corrections to database schema

--- database_init.sql
+++ database_init.sql
@@ -32,27 +32,27 @@ CREATE TABLE recommendations (
   title varchar(50),
   body text, --CHECK (length(body) > 100),--should we have a character limit?
   -- how do you even save photos?!
-  user_id integer NOT NULL REFERENCES users (id) ON DELETE CASCADE
+  user_id integer NOT NULL REFERENCES users (id) ON DELETE CASCADE,
   categories text
 );
 
-CREATE TABLE destination (
+CREATE TABLE destinations (
   id serial PRIMARY KEY,
   city varchar(50),
   country varchar(50),
   description varchar(50)
-  )
+  );
 
 INSERT INTO users (first_name, last_name, display_name) VALUES ('Nick', 'Pizzigati', 'Nico');
 INSERT INTO users (first_name, last_name, display_name) VALUES ('Leeya', 'Davis', 'Leelee');
 INSERT INTO users (first_name, last_name, display_name) VALUES ('Dhana', 'McTavish', 'Day');
 INSERT INTO users (first_name, last_name, display_name) VALUES ('Teresa', 'Garcia', 'Tere');
 
-INSERT INTO destination (city, country, description) VALUES ('Guanajuato', 'Mexico', '');
-INSERT INTO destination (city, country, description) VALUES ('Tulum', 'Mexico', '');
-INSERT INTO destination (city, country, description) VALUES ('Seoul', 'South Korea', '');
-INSERT INTO destination (city, country, description) VALUES ('Rome', 'Italy', '');
-INSERT INTO destination (city, country, description) VALUES ('Kuala Lumpar', 'Malaysia', '');
+INSERT INTO destinations (city, country, description) VALUES ('Guanajuato', 'Mexico', '');
+INSERT INTO destinations (city, country, description) VALUES ('Tulum', 'Mexico', '');
+INSERT INTO destinations (city, country, description) VALUES ('Seoul', 'South Korea', '');
+INSERT INTO destinations (city, country, description) VALUES ('Rome', 'Italy', '');
+INSERT INTO destinations (city, country, description) VALUES ('Kuala Lumpar', 'Malaysia', '');
 
 INSERT INTO recommendations (user_id, destination_id, title, body)
 VALUES (1, 1, 'Coffee', 'Coffee at Café Tal'),
